35bce250b8f6dfb3be43baaf768b8dbfc28e248c
AceRy07 <ardaakar177@gmail.com>
2025-10-28 22:34:41 +0300

    Factory Design Pattern Add

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -10,6 +10,79 @@ namespace FactoryMethod
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    // Factory Method Pattern
+    // DataAccessLayer Abstract Factory Pattern ile benzerlik gösterir.
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    // DataAccessLayer Concrete Factory Pattern ile benzerlik gösterir.
+    public class LoggerFactory : ILoggerFactory
+        // Biz yazılımda classlar sade (çıplak inheritace) bir interface'den inherit edilmelidir.
+    {
+        // Factory Method budur.
+        public ILogger CreateLogger()
+        {
+            // Business to decide factory
+            return new EdLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        // Factory Method budur.
+        public ILogger CreateLogger()
+        {
+            // Business to decide factory
+            return new LogfNetLogger();
+        }
+    }
+
+    // VeriTabanı gibi düşünülebilir.
+    // ILogger interface'i farklı loglama teknikleri için kullanılabilir.
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    // Kulanıcı arayüzüne loglama
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+    }
+
+    public class LogfNetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with LogfNetLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
         }
     }
 }
